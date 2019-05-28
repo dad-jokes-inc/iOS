80c734bad6552e7c1a13d8e8bf958fce7a4df4b0
Hayden Hastings <haydenhastings32@gmail.com>
2019-05-28 16:08:16 -0600

    Added AppearanceHelper file. Added code in LoginViewController.

--- DadJokes/View Controllers/LoginViewController.swift
+++ DadJokes/View Controllers/LoginViewController.swift
@@ -9,11 +9,47 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    // MARK: - Properties
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    // MARK: - Enums
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupAppearance()
+    }
+    
+    // MARK: - Methods
+    
+    func setupAppearance() {
+        view.backgroundColor = AppearanceHelper.dadJokesBlue
+        
+        emailTextField.layer.borderColor = AppearanceHelper.dadJokesGreyishWhite.cgColor
+        emailTextField.attributedPlaceholder = NSAttributedString(string: "Email:", attributes: [NSAttributedString.Key.foregroundColor: AppearanceHelper.dadJokesGreyishWhite])
+        emailTextField.layer.borderWidth = 1
+        emailTextField.layer.cornerRadius = 10
+        emailTextField.backgroundColor = .clear
+        
+        passwordTextField.layer.borderColor = AppearanceHelper.dadJokesGreyishWhite.cgColor
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: "Password:", attributes: [NSAttributedString.Key.foregroundColor: AppearanceHelper.dadJokesGreyishWhite])
+        passwordTextField.layer.borderWidth = 1
+        passwordTextField.layer.cornerRadius = 10
+        passwordTextField.backgroundColor = .clear
+        
+        button.layer.backgroundColor = AppearanceHelper.dadJokesYellow.cgColor
+        button.layer.cornerRadius = 10
+        button.tintColor = AppearanceHelper.dadJokesBlue
+        
+        noAccountLabel.textColor = AppearanceHelper.dadJokesGreyishWhite
+        
+        entryMethodSegmentedControl.layer.backgroundColor = AppearanceHelper.dadJokesYellow.cgColor
+        entryMethodSegmentedControl.layer.cornerRadius = 10
+        entryMethodSegmentedControl.layer.borderWidth = 1
+        entryMethodSegmentedControl.layer.borderColor = AppearanceHelper.dadJokesYellow.cgColor
+        entryMethodSegmentedControl.tintColor = AppearanceHelper.dadJokesBlue
     }
     
     // MARK: - Actions
@@ -30,4 +66,5 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var button: UIButton!
     @IBOutlet weak var entryMethodSegmentedControl: UISegmentedControl!
     
+    @IBOutlet weak var noAccountLabel: UILabel!
 }
---
+++ Helper/AppearanceHelper.swift
@@ -0,0 +1,16 @@
+//
+//  AppearanceHelper.swift
+//  DadJokes
+//
+//  Created by Hayden Hastings on 5/28/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+enum AppearanceHelper {
+    
+    static let dadJokesYellow = UIColor(red: 248 / 255, green: 209 / 255, blue: 77 / 255, alpha: 1)
+    static let dadJokesBlue = UIColor(red: 0 / 255, green: 9 / 255, blue: 71 / 255, alpha: 1)
+    static let dadJokesGreyishWhite = UIColor(red: 218 / 255, green: 221 / 255, blue: 224 / 255, alpha: 1)
+}
