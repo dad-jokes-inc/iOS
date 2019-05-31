b128541373bf5b3f52b85b09c51843d0382939bd
Hayden Hastings <haydenhastings32@gmail.com>
2019-05-31 10:47:52 -0600

    Fixed keyboard and text label color.

--- DadJokes/View Controllers/JokesDetailViewController.swift
+++ DadJokes/View Controllers/JokesDetailViewController.swift
@@ -72,8 +72,12 @@ class JokesDetailViewController: UIViewController, UITextViewDelegate {
         jokeTextView.layer.borderWidth = 1
         jokeTextView.layer.borderColor = AppearanceHelper.dadJokesYellow.cgColor
         jokeTextView.textColor = .white
+        jokeTextView.tintColor = AppearanceHelper.dadJokesYellow
+        
+        textLeabel.textColor = .white
         
         saveButton.tintColor = AppearanceHelper.dadJokesGreyishWhite
+        savePrivateJokeButton.tintColor = AppearanceHelper.dadJokesGreyishWhite
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -85,17 +89,19 @@ class JokesDetailViewController: UIViewController, UITextViewDelegate {
     
     @objc func keyboardWillChange(notification: Notification) {
         
-        guard let keyboardRect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
-            return
-        }
+        view.frame.origin.y = -100
         
-        if notification.name == UIResponder.keyboardWillShowNotification ||
-            notification.name == UIResponder.keyboardWillChangeFrameNotification {
-            
-            view.frame.origin.y = -keyboardRect.height
-        } else {
-            view.frame.origin.y = 0
-        }
+//        guard let keyboardRect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+//            return
+//        }
+//
+//        if notification.name == UIResponder.keyboardWillShowNotification ||
+//            notification.name == UIResponder.keyboardWillChangeFrameNotification {
+//
+//            view.frame.origin.y = -keyboardRect.height
+//        } else {
+//            view.frame.origin.y = 0
+//        }
     }
     
     deinit {
@@ -148,4 +154,5 @@ class JokesDetailViewController: UIViewController, UITextViewDelegate {
     @IBOutlet weak var jokeTextView: UITextView!
     @IBOutlet weak var saveButton: UIButton!
     @IBOutlet weak var savePrivateJokeButton: UIButton!
+    @IBOutlet weak var textLeabel: UILabel!
 }
